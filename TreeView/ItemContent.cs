faeeab2cacdebc56625808bb6e4f1dd58c17f694
Stefan Lörwald <stefan.loerwald@excubo-ag.com>
2020-08-07 11:49:31 +0200

    simplify parameters

---
+++ TreeView/ItemContent.cs
@@ -0,0 +1,17 @@
+﻿namespace Excubo.Blazor.TreeViews
+{
+    public struct ItemContent<T>
+    {
+        public int Level { get; set; }
+        public T Item { get; set; }
+        public ItemContent(int level, T item)
+        {
+            Level = level;
+            Item = item;
+        }
+        public static implicit operator ItemContent<T>((int Level, T Item) value)
+        {
+            return new ItemContent<T>(value.Level, value.Item);
+        }
+    }
+}
---
+++ TreeView/__Internal/OrderedListBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace Excubo.Blazor.TreeViews.__Internal
+{
+    public class OrderedListBase<T> : ComponentBase
+    {
+        [CascadingParameter] private TreeView<T> TreeView { get; set; }
+        protected RenderFragment<ItemContent<T>> ItemTemplate => TreeView.ItemTemplate;
+        protected Func<IEnumerable<T>, IEnumerable<T>> SortBy => TreeView.SortBy;
+        [Parameter] public int Level { get; set; }
+        [Parameter] public bool Collapsed { get; set; }
+    }
+}
