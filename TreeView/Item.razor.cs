ad667296cf45d531e8213e9340637ec44cf63b7d
Stefan Lörwald <stefan.loerwald@gmail.com>
2021-01-19 12:28:09 +0100

    added MarkupTreeView [deploy]

---
+++ TreeView/Item.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace Excubo.Blazor.TreeViews
+{
+    /// <summary>
+    /// An item of a <see cref="MarkupTreeView"/>
+    /// </summary>
+    public partial class Item : IDisposable
+    {
+        [CascadingParameter] public MarkupTreeView TreeView { get; set; }
+        [CascadingParameter] public Item Parent { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        private bool Collapsed { get; set; }
+        internal readonly HashSet<Item> Children = new HashSet<Item>();
+        protected override void OnInitialized()
+        {
+            if (TreeView.InitiallyCollapsed)
+            {
+                Collapsed = true;
+            }
+            if (Parent != null)
+            {
+                Parent.Children.Add(this);
+                Parent.StateHasChanged();
+            }
+            base.OnInitialized();
+        }
+        public void Dispose()
+        {
+            if (Parent != null)
+            {
+                Parent.Children.Remove(this);
+            }
+        }
+    }
+}
---
+++ TreeView/MarkupTreeView.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Excubo.Blazor.TreeViews
+{
+    public partial class MarkupTreeView : ComponentBase
+    {
+        /// <summary>
+        /// Optional class(es) that are applied to every &lt;ul&gt; tag in the tree view.
+        /// </summary>
+        [Parameter] public string ListClass { get; set; }
+        /// <summary>
+        /// Optional class(es) that are applied to every &lt;li&gt; tag in the tree view.
+        /// </summary>
+        [Parameter] public string ItemClass { get; set; }
+        /// <summary>
+        /// When set to true, all items are initially collapsed.
+        /// </summary>
+        [Parameter] public bool InitiallyCollapsed { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
