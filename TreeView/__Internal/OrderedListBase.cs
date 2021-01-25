5656a9e15cd391a9e143b2ea9710bef2ef812a99
pab <pa.burdet@arx.one>
2021-01-25 10:58:00 +0100

    Async tree view and OrderedListByGetchildrenAsync

--- TreeView/__Internal/OrderedListBase.cs
+++ TreeView/__Internal/OrderedListBase.cs
@@ -8,6 +8,7 @@ namespace Excubo.Blazor.TreeViews.__Internal
     public class OrderedListBase<T> : ComponentBase
     {
         [CascadingParameter] protected TreeView<T> TreeView { get; set; }
+        [CascadingParameter] protected AsyncTreeView<T> AsyncTreeView { get; set; }
         protected RenderFragment<ItemContent<T>> ItemTemplate => TreeView.ItemTemplate;
         protected Func<IEnumerable<T>, IEnumerable<T>> FilterBy => TreeView.FilterBy;
         protected Func<IEnumerable<T>, IEnumerable<T>> SortBy => TreeView.SortBy;
