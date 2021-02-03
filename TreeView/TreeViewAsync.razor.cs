71177c21e880dd16754e9b4edc3c26ed851ef59c
Stefan Lörwald <stefan.loerwald@gmail.com>
2021-02-03 10:29:52 +0100

    move code into code-behind

---
+++ TreeView/TreeViewAsync.razor.cs
@@ -0,0 +1,34 @@
+﻿using Excubo.Blazor.TreeViews.__Internal;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Excubo.Blazor.TreeViews
+{
+    public partial class TreeViewAsync<T> : TreeViewBase<T>
+    {
+
+        [Parameter]
+        public T RootItem { get; set; }
+
+        [Parameter]
+        public Func<T, Task<List<T>>> GetChildrenAsync { get; set; }
+
+        [Parameter]
+        public Func<T, Task<bool>> HasChildrenAsync { get; set; }
+
+        [Parameter]
+        public RenderFragment LoadingTemplate { get; set; } = (builder) =>
+        {
+            builder.OpenComponent<DefaultLoadingTemplate>(0);
+            builder.CloseComponent();
+        };
+
+        protected override void OnParametersSet()
+        {
+            InitiallyCollapsed = true;
+            base.OnParametersSet();
+        }
+    }
+}
