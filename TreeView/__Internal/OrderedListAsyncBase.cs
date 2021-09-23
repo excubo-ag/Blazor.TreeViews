f51b87520d60ff0e1c2a7d40a8a5132a20ba6570
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2021-09-23 10:10:35 +0200

    [deploy] Enable ListClass feature on async TVs (fixes #34)

--- TreeView/__Internal/OrderedListAsyncBase.cs
+++ TreeView/__Internal/OrderedListAsyncBase.cs
@@ -10,6 +10,7 @@ namespace Excubo.Blazor.TreeViews.__Internal
         protected RenderFragment<ItemContent<T>> ItemTemplate => TreeViewAsync.ItemTemplate;
         protected Func<IEnumerable<T>, IEnumerable<T>> FilterBy => TreeViewAsync.FilterBy;
         protected Func<IEnumerable<T>, IEnumerable<T>> SortBy => TreeViewAsync.SortBy;
+        protected string Class => TreeViewAsync?.ListClass;
         [Parameter] public int Level { get; set; }
         [Parameter] public bool Collapsed { get; set; }
     }
